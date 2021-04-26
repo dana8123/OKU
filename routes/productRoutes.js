7c661f90d824c1c4f769906a4a1bac6e4dc70ed7
=danaisboss <yjk9313@gmail.com>
2021-04-26 17:39:41 +0900

    Fix routes export

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,11 @@
 // 상품관련 API
-const express = require('express');
-export const productRouter = express.Router();
-const { upload } = require('../controllers/usercontroller');
+const express = require("express");
+const productRouter = express.Router();
+const { upload } = require("../controllers/usercontroller");
 
 // 잘연결됐는지 확인
-productRouter.get("/test",test);
+productRouter.get("/test", test);
 
-productRouter.get("/",upload);
+productRouter.get("/", upload);
+
+module.exports = productRouter;
