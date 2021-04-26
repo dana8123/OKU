c2910b9cd2b1ce8e4d34fa9f54beb9a222a741e5
=danaisboss <yjk9313@gmail.com>
2021-04-26 17:51:44 +0900

    Fix module exports

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,1 +1,6 @@
 // 상품 리스트 프론트에 내려주는것들 여기다
+
+exports.test = async (req, res) => {
+	res.send("잘 연결이 되었습니다!");
+	console.log("연결이 잘 되었습니다!");
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 // 유저관련 API
-const express = require('express');
+const express = require("express");
 //이부분도 제가 임의로 넣은값이니 추후 수정이 필요하면 수정해주세요.
 // 이런식으로 컨트롤러 안에 사용하려는 함수들 일일히 들고와야함!
-const { login, register } = require('../controllers/usercontroller');
-export const userRouter = express.Router();
+const { login, register } = require("../controllers/usercontroller");
+const userRouter = express.Router();
 
-//두번째 인자의 경우, 임의로 넣어놓은겁니다!  
-userRouter.post('/login', login);
-userRouter.post('/register', register);
+//두번째 인자의 경우, 임의로 넣어놓은겁니다!
+userRouter.post("/login", login);
+userRouter.post("/register", register);
+
+module.exports = { userRouter };
