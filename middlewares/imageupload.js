3074445bb3fa5d733b7beb2b0a854b0a96f1c501
=danaisboss <yjk9313@gmail.com>
2021-04-28 21:02:18 +0900

    Fix, image save path with multer

--- middlewares/imageupload.js
+++ middlewares/imageupload.js
@@ -3,9 +3,9 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-	// destination: function (req, file, cb) {
-	// 	cb(null, "/uploads");
-	// },
+	destination: function (req, file, cb) {
+		cb(null, "uploads/");
+	},
 	filename: function (req, file, cb) {
 		const ext = path.extname(file.originalname);
 		cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
