bc206730acc2daf6624b2d1bc0a4a2d8e4e12a9d
=danaisboss <yjk9313@gmail.com>
2021-04-28 20:45:21 +0900

    Fix upload img, with multer

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,5 @@
 // 상품등록관련
-require('dotenv').config();
+require("dotenv").config();
 const user = require("../schema/user");
 const Product = require("../schema/product");
 // const user = require("../schema/user");
@@ -7,55 +7,51 @@ const { authMiddlesware } = require("../middlewares/auth-middleware.js");
 const { upload } = require("../middlewares/imageupload.js");
 
 exports.test = async (req, res) => {
-    console.log(res.locals.user);
-    res.send(res.locals.user);
-}
+	console.log(res.locals.user);
+	res.send(res.locals.user);
+};
 
 exports.productpost = async (req, res) => {
-    const {
-        title,
-        img,
-        nickname,
-        lowbid,
-        sucbid,
-        state,
-        description,
-        tag,
-        bigCategory,
-        smallCategory,
-        region,
-        deliveryprice,
-        deadline
-    } = req.body;
-
-    try {
-        await Product.create({
-            title: title,
-            img: img,
-            nickname: nickname,
-            lowBid: lowbid,
-            sucBid: sucbid,
-            state: state,
-            description: description,
-            tag: tag,
-            bigCategory: bigCategory,
-            smallCategory: smallCategory,
-            region: region,
-            deliveryPrice: deliveryprice,
-            deadLine: deadline
-        });
-
-        res.send({ msg: "상품이 등록되었습니다" });
-    } catch (error) {
-        res.send({ msg: "상품이 등록에 실패하였습니다." });
-    }
-}
+	const {
+		title,
+		nickname,
+		lowbid,
+		sucbid,
+		state,
+		description,
+		tag,
+		bigCategory,
+		smallCategory,
+		region,
+		deliveryprice,
+		deadline,
+	} = req.body;
+	const file = req.file.path;
+	console.log(req.file.path);
+	try {
+		await Product.create({
+			title,
+			img: file,
+			nickname,
+			lowBid: lowbid,
+			sucBid: sucbid,
+			state: state,
+			description: description,
+			tag: tag,
+			bigCategory: bigCategory,
+			smallCategory: smallCategory,
+			region: region,
+			deliveryPrice: deliveryprice,
+			deadLine: deadline,
+		});
+		res.send({ msg: "상품이 등록되었습니다" });
+	} catch (error) {
+		res.send({ msg: "상품이 등록에 실패하였습니다.", error });
+	}
+};
 
 exports.bidding = async (req, res) => {
-    // const user = res.locals.user;
-    try {
-
-    } catch (error) {
-
-    }
-}
+	// const user = res.locals.user;
+	try {
+	} catch (error) {}
+};
--- middlewares/imageupload.js
+++ middlewares/imageupload.js
@@ -1,27 +1,39 @@
-require('dotenv').config();
-const path = require("path");
+require("dotenv").config();
 const multer = require("multer");
+const path = require("path");
 
-exports.upload = (req, res, next) => {
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-           cb(null, 'public/');
-        },
-        filename: function (req, file, cb) {
-           let ex = file.originalname.split('.');
-           console.log(ex) 
-           cb(null, 'img' + Date.now() + parseInt(Math.random() * (99 - 10) + 10) + '.' + ex[ex.length - 1]);
-        }
-     });
+const storage = multer.diskStorage({
+	// destination: function (req, file, cb) {
+	// 	cb(null, "/uploads");
+	// },
+	filename: function (req, file, cb) {
+		const ext = path.extname(file.originalname);
+		cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+	},
+});
 
-     function fileFilter(req, file, cb) {
-        const fileType = file.mimetype.split('/')[0] == 'image';
-        if (fileType) cb(null, true);
-        else cb(null, false);
-     }
-     
-     const upload = multer({
-        storage: storage,
-        fileFilter: fileFilter
-     });
-}
+exports.upload = multer({ storage });
+
+// exports.upload = (req, res, next) => {
+//     const storage = multer.diskStorage({
+//         destination: function (req, file, cb) {
+//            cb(null, 'public/');
+//         },
+//         filename: function (req, file, cb) {
+//            let ex = file.originalname.split('.');
+//            console.log(ex)
+//            cb(null, 'img' + Date.now() + parseInt(Math.random() * (99 - 10) + 10) + '.' + ex[ex.length - 1]);
+//         }
+//      });
+
+//      function fileFilter(req, file, cb) {
+//         const fileType = file.mimetype.split('/')[0] == 'image';
+//         if (fileType) cb(null, true);
+//         else cb(null, false);
+//      }
+
+//      const upload = multer({
+//         storage: storage,
+//         fileFilter: fileFilter
+//      });
+// }
