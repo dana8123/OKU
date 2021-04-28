a6ef958ba8fefef230e4b253441b89bc156aabe9
silano_08 <henzel1013@gmail.com>
2021-04-28 11:14:51 +0900

    [okay]

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,14 +8,18 @@ const { bidding,productpost,test } = require("../controllers/postcontroller");
 const { authMiddlesware } = require("../middlewares/auth-middleware.js");
 const { upload } = require("../middlewares/imageupload.js");
 
-// 이미지 등록용 테스트
-productRouter.post("/test",upload,test)
+// 테스트용 api
+productRouter.post("/test", authMiddlesware,test)
 
 // 상품등록
+// 미들웨어 붙이고 로그인처리 필요
 productRouter.post("/",productpost);
 
-// 상품입찰
+// 상품 입찰
 productRouter.post("/auction",authMiddlesware,bidding);
+// 상품 즉시 낙찰
+productRouter.post("/auction",authMiddlesware,bidding);
+
 
 // 대분류에 따른 상품리스트
 productRouter.get("/product/:bigCategory",bigCate);
