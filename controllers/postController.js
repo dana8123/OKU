f8d6b729dc26f34a730dfd9f9534cfef828830fc
silano_08 <henzel1013@gmail.com>
2021-04-29 11:27:46 +0900

    [popular]

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,64 +8,77 @@ const { upload } = require("../middlewares/imageupload.js");
 
 exports.productpost = async (req, res) => {
     let image = '';
-    if(req["file"]){
+    if (req["file"]) {
         images = req.file.filename
-        image = `http://${process.env.DB_SERVER}:${process.env.DB_PORT}/` + req.file.filename  
-      }
+        image = `http://${process.env.DB_SERVER}:${process.env.DB_PORT}/` + req.file.filename
+    }
 
-	const {
-		title,
-		nickname,
-		lowbid,
-		sucbid,
-		state,
-		description,
-		tag,
-		bigCategory,
-		smallCategory,
-		region,
-		deliveryprice,
-		deadline,
-	} = req.body;
+    const {
+        title,
+        nickname,
+        lowbid,
+        sucbid,
+        state,
+        description,
+        tag,
+        bigCategory,
+        smallCategory,
+        region,
+        deliveryprice,
+        deadline,
+    } = req.body;
 
-	const file = req.file.path;
-	console.log(req.file.path);
-	try {
-		await Product.create({
-			title,
-			img: image,
-			nickname,
-			lowBid: lowbid,
-			sucBid: sucbid,
-			state: state,
-			description: description,
-			tag: tag,
-			bigCategory: bigCategory,
-			smallCategory: smallCategory,
-			region: region,
-			deliveryPrice: deliveryprice,
-			deadLine: deadline,
-		});
-		res.send({ msg: "상품이 등록되었습니다" });
-	} catch (error) {
-		res.send({ msg: "상품이 등록에 실패하였습니다.", error });
-	}
+    const file = req.file.path;
+    console.log(req.file.path);
+    try {
+        await Product.create({
+            title,
+            img: image,
+            nickname,
+            lowBid: lowbid,
+            sucBid: sucbid,
+            state: state,
+            description: description,
+            tag: tag,
+            bigCategory: bigCategory,
+            smallCategory: smallCategory,
+            region: region,
+            deliveryPrice: deliveryprice,
+            deadLine: deadline,
+        });
+        res.send({ msg: "상품이 등록되었습니다" });
+    } catch (error) {
+        res.send({ msg: "상품이 등록에 실패하였습니다.", error });
+    }
 };
 
-exports.detail = async(req,res)=>{
-    // res.send(req.params);
-    // console.log(req.params["id"]);
-    
+exports.popular = async (req, res) => {
+    const a = await Product.find({});
+    console.log(a);
     try {
-        const product = await Product.findOne({_id:req.params["id"]},{__v:0});
-        res.json({okay:true,result:product});
+        const a = await Product.find({}).sort("-views").limit(3);
+        console.log(a);
+        res.send({ result: a });
     } catch (error) {
-        res.send({okay:false});
+        res.send({ result:"왜 다른라우터의결과값이나오나요?" });
     }
-}
+};
+
+//
+
+exports.detail = async (req, res) => {
+    try {
+        const product = await Product.findOneAndUpdate({ _id: req.params["id"] }, { $inc: { views: 1 } }, { __v: 0 });
+        res.json({ okay: true, result: product });
+    } catch (error) {
+        res.send({ okay: false });
+    }
+};
+
+///
 
 exports.bidding = async (req, res) => {
-	// const user = res.locals.user;
-	try {
-	} catch (error) {}
+    // const user = res.locals.user;
+    try {
+    } catch (error) { }
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,7 @@ const productRouter = express.Router();
 const multer = require("multer");
 const { upload } = require("../middlewares/imageupload.js");
 const { bigCate, smallCate } = require("../controllers/productController");
-const { bidding, productpost, detail } = require("../controllers/postController");
+const { bidding, productpost, detail, popular } = require("../controllers/postController");
 const { authMiddlesware } = require("../middlewares/auth-middleware.js");
 
 // 테스트용 api
@@ -13,20 +13,24 @@ const { authMiddlesware } = require("../middlewares/auth-middleware.js");
 
 // 상품등록
 // 미들웨어 붙이고 로그인처리 필요
-productRouter.post("/", upload.single("img"), productpost);
+productRouter.post("/", upload.single("img"),productpost);
 
 // 상품상세보기
-productRouter.get("/:id",detail);
+productRouter.get("/detail/:id",detail);
+
+// 실시간 인기상품 리스트
+productRouter.get("/popularlist",popular);
+
+// 대분류에 따른 상품리스트
+productRouter.get("/product/:bigCategory", bigCate);
+// 중분류에 따른 상품리스트
+productRouter.get("/product/:bigCategory/:smallCategory", smallCate);
 
 // 상품 입찰
 productRouter.post("/auction", authMiddlesware, bidding);
 // 상품 즉시 낙찰
 productRouter.post("/auction", authMiddlesware, bidding);
 
-// 대분류에 따른 상품리스트
-productRouter.get("/product/:bigCategory", bigCate);
 
-// 중분류에 따른 상품리스트
-productRouter.get("/product/:bigCategory/:smallCategory", smallCate);
 
 module.exports = { productRouter };
