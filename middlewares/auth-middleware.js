b2a1018602385381051b24cb087a82a2a2ba82d3
=danaisboss <yjk9313@gmail.com>
2021-05-02 22:28:43 +0900

    Delete check token type

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,30 +1,29 @@
-const jwt = require('jsonwebtoken');
-const User = require('../schema/user');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const User = require("../schema/user");
+require("dotenv").config();
 
 // module.exports < 이렇게 export했다가 오지게 에러나서
 // 이렇게 수정함
 
 exports.authMiddlesware = (req, res, next) => {
-   try {
-      const { authorization } = req.headers;
-      const [tokenType, tokenValue] = authorization.split(" ");
-
-      if (tokenType !== 'Bearer') {
-         res.status(400).send({
-            err: "로그인 후 이용 가능한 기능입니다.",
-         });
-         return;
-      }
-      const { userId } = jwt.verify(tokenValue, process.env.SECRET_KEY);
-      User.findOne({ userId: userId }).then((user) => {
-         res.locals.user = user;
-         next();
-      });
-   } catch (error) {
-      res.json({
-         mss: 'not_login'
-      });
-      return;
-   }
+	try {
+		const { access_token } = req.headers;
+		// const [tokenType, tokenValue] = access_token.split(" ");
+		// if (tokenType !== "Bearer") {
+		// 	res.status(400).send({
+		// 		err: "로그인 후 이용 가능한 기능입니다.",
+		// 	});
+		// 	return;
+		// }
+		const { email } = jwt.verify(access_token, process.env.SECRET_KEY);
+		User.findOne({ email }).then((user) => {
+			res.locals.user = user;
+			next();
+		});
+	} catch (error) {
+		res.json({
+			msg: "not_login",
+		});
+		return;
+	}
 };
