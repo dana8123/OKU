198345f4504454d239a55c866c85cce5fb36cf86
=danaisboss <yjk9313@gmail.com>
2021-05-07 02:25:24 +0900

    입찰조건 추가

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -15,26 +15,32 @@ exports.bid = async (req, res) => {
 		let result = true;
 		const { bid } = req.body;
 		const product = await Product.findById(id);
-		const price = PriceHistory.find().where("productId").equals();
-		//console.log(price);
+		const bidList = await PriceHistory.find({ productId: id });
+		//console.log(bidList);
+		//console.log("===두번째디버깅===", price);
+
+		const lowBid = await product.lowBid;
+		console.log("시작가", lowBid);
 
 		//입찰 시 시작가보다 낮거나 같을 때
-		const lowBid = await product.lowBid;
-		console.log("최저입찰가", lowBid);
 		if (lowBid >= bid) {
 			result = false;
 			return res.status(403).send({ result });
 		}
-		//TODO:입찰 시 이전 입찰가보다 낮거나 같을 때
-		// if (pricehistory.currentPrice && product.currentPrice) {
-		// 	result = false;
-		// 	return res.status(403).send({ result });
-		// }
+		//경매 기간이 지났을 경우
+		let now = new Date();
+		if (product.deadLine < now) {
+			result = false;
+			return res.status(403).send({ result });
+		}
+		//입찰 시 이전 입찰가보다 낮거나 같을 때
+		if (bidList[0] && bidList[bidList.length - 1].bid >= bid) {
+			result = false;
+			return res.status(403).send({ result });
+		}
 		result = await pricehistory.create({
-			info: {
-				userId: user["nickname"],
-				bid,
-			},
+			userId: user["_id"],
+			bid,
 			productId: id,
 		});
 		res.send({ result });
