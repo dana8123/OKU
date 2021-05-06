5e7767db29a2894a3a31ec95a93f801023b4346a
=danaisboss <yjk9313@gmail.com>
2021-05-06 23:37:45 +0900

    Add bid, in socket controller

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -4,23 +4,40 @@ const PriceHistory = require("../schema/pricehistory");
 const User = require("../schema/user");
 const Like = require("../schema/like");
 const { authMiddlesware } = require("../middlewares/auth-middleware.js");
+const pricehistory = require("../schema/pricehistory");
 
 exports.bid = async (req, res) => {
-	const { params: id } = req;
-	let result;
+	const user = res.locals.user;
+	const { id } = req.params;
 	try {
+		let result = true;
 		const { bid } = req.body;
-		const product = Product.findById(id);
-		//const pricehistory = priceHistory.findOne({ productId: id });
-    const recentPrice = Pricehistory.aggregate([
-      
-    ])
+		const product = await Product.findById(id);
+		const price = PriceHistory.find().where("productId").equals();
+		//console.log(price);
+
 		//입찰 시 시작가보다 낮거나 같을 때
-		if (product.lowbid >= bid) {
+		const lowBid = await product.lowBid;
+		console.log("최저입찰가", lowBid);
+		if (lowBid >= bid) {
 			result = false;
 			return res.status(403).send({ result });
 		}
-		//입찰 시 이전 입찰가보다 낮거나 같을 때
-		if ( pricehistory.currentPrice && product.currentPrice )
-	} catch (error) {}
+		//TODO:입찰 시 이전 입찰가보다 낮거나 같을 때
+		// if (pricehistory.currentPrice && product.currentPrice) {
+		// 	result = false;
+		// 	return res.status(403).send({ result });
+		// }
+		result = await pricehistory.create({
+			info: {
+				userId: user["nickname"],
+				bid,
+			},
+			productId: id,
+		});
+		res.send({ result });
+	} catch (error) {
+		console.error(error);
+		res.send({ error });
+	}
 };
--- schema/pricehistory.js
+++ schema/pricehistory.js
@@ -2,18 +2,21 @@ const mongoose = require("mongoose");
 const { Schema, model, Types } = mongoose;
 
 const priceHistory = new Schema({
-	userId: {
-		type: String,
-	},
-	productId: {
-		type: ObjectId(),
-	},
-	currentPrice: {
-		type: Number,
-	},
-	createAt: {
-		type: Date,
-		default: Date.now(),
+	// userId: {
+	// 	type: String,
+	// },
+	// productId: String,
+	// currentPrice: {
+	// 	type: Number,
+	// },
+	// createAt: {
+	// 	type: Date,
+	// 	default: Date.now(),
+	// },
+	productId: Object,
+	info: {
+		userId: String,
+		bid: Number,
 	},
 });
 
