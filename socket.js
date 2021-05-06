b115110b0bb426dec3af3768514eb6211f18de8b
=danaisboss <yjk9313@gmail.com>
2021-05-06 11:10:36 +0900

    Prac for namespace

--- socket.js
+++ socket.js
@@ -2,12 +2,19 @@
 const SocketIO = require("socket.io");
 
 //app.js와 websocket을 연결하는 작업
-module.exports = (app) => {
+module.exports = (server, app) => {
 	//path는 클라이언트쪽 코드와 맞춰놔야함(현재입력값이 디폴트값임.)
-	const io = SocketIO(app, { path: "/socket.io" });
+	const io = SocketIO(server, { path: "/socket.io" });
+	app.set("io", io);
+	//namespace 지정
+	const bid = io.of("/bid");
+
 	//server-side
-	io.on("connection", (socket) => {
+	bid.on("connection", (socket) => {
+		socket.on("order:list", () => {
+			console.log("orderlist event");
+		});
 		socket.emit("hello", "하하하");
-		console.log(socket.id);
+		console.log("order 네임스페이스에 접속!");
 	});
 };
