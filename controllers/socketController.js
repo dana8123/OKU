86d9178e1bbc3682636cf0a7b73cc91b5f60c55f
=danaisboss <yjk9313@gmail.com>
2021-05-06 14:01:08 +0900

    Settings for bid

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,5 +1,26 @@
 // 소켓 리스트 관련
 const Product = require("../schema/product");
+const PriceHistory = require("../schema/pricehistory");
 const User = require("../schema/user");
 const Like = require("../schema/like");
 const { authMiddlesware } = require("../middlewares/auth-middleware.js");
+
+exports.bid = async (req, res) => {
+	const { params: id } = req;
+	let result;
+	try {
+		const { bid } = req.body;
+		const product = Product.findById(id);
+		//const pricehistory = priceHistory.findOne({ productId: id });
+    const recentPrice = Pricehistory.aggregate([
+      
+    ])
+		//입찰 시 시작가보다 낮거나 같을 때
+		if (product.lowbid >= bid) {
+			result = false;
+			return res.status(403).send({ result });
+		}
+		//입찰 시 이전 입찰가보다 낮거나 같을 때
+		if ( pricehistory.currentPrice && product.currentPrice )
+	} catch (error) {}
+};
--- schema/pricehistory.js
+++ schema/pricehistory.js
@@ -1,16 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema, model, Types } = mongoose;
 
 const priceHistory = new Schema({
-    userId:{
-        type: String
-    },
-    productId: {
-      type: ObjectId(),
-    },
-    currentPrice:{
-      type: Number,
-    },
+	userId: {
+		type: String,
+	},
+	productId: {
+		type: ObjectId(),
+	},
+	currentPrice: {
+		type: Number,
+	},
+	createAt: {
+		type: Date,
+		default: Date.now(),
+	},
 });
 
-module.exports = mongoose.model('PrieceHistory', priceHistory);
+module.exports = mongoose.model("PrieceHistory", priceHistory);
