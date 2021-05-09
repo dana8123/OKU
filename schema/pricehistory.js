7b904d5fd1824f65e1231a5317b2d1556f260a64
=danaisboss <yjk9313@gmail.com>
2021-05-09 16:35:55 +0900

    낙찰자 정보 추가

--- schema/pricehistory.js
+++ schema/pricehistory.js
@@ -2,9 +2,14 @@ const mongoose = require("mongoose");
 const { Schema, model, Types } = mongoose;
 const priceHistory = new Schema({
 	productId: Object,
+	seller: Object,
 	userId: Object,
-	nickName:String,
+	nickName: String,
 	bid: Number,
+	soldBy: {
+		type: Object,
+		default: undefined,
+	},
 	createAt: {
 		type: Date,
 		default: Date.now,
