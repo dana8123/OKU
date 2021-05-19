33bc0f9bee575b66d71d8471e79775d900638be2
danaisboss <yjk9313@gmail.com>
2021-05-19 20:54:01 +0900

    Fix, kakao Email 받는 코드

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -12,16 +12,17 @@ passport.use(
 		},
 		async (accessToken, refreshToken, profile, done) => {
 			const {
-				_json: { id, properties },
+				_json: { id, properties, kakao_account },
 			} = profile;
 			try {
 				const user = await User.findOne({ kakaoId: id });
 				if (user) {
+					console.log("유저프로퍼티", profile);
 					return done(null, user);
 				}
 				const newUser = await User.create({
 					kakaoId: id,
-					email: properties.email,
+					email: kakao_account.email,
 					nickname: properties.nickname,
 				});
 				await newUser.save();
